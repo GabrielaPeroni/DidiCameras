ec1a7f01a0000d60d395e62f11277c9a4fbc12cc
GabrielaPeroni <gabriela.campos.dev@gmail.com>
2025-06-08 13:34:57 -0300

    feat: add CORS support by including django-cors-headers in settings and requirements

--- DidiCameras/settings.py
+++ DidiCameras/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'axes',
 ]
 
@@ -59,8 +60,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     # AxesMiddleware should be the last middleware in the MIDDLEWARE list
     'axes.middleware.AxesMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = 'DidiCameras.urls'
