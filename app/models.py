06f5959ea8154388e16e418bb172f3e254dab82f
Andrey <andreyfernandes1361@gmail.com>
2025-06-16 21:42:37 -0300

    add new recording configuration variables

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
 class Camera(models.Model):
@@ -22,7 +23,12 @@ class Recording(models.Model):
         return f"{self.camera.name} - {self.timestamp.strftime('%d-%m-%Y %H:%M')}"
 
 class FFmpegConfig(models.Model):
-    recording_duration = models.PositiveIntegerField(default=5) # Minutes by default
+    recording_duration = models.PositiveIntegerField(default=5) # Minutes
+    recording_resolution = models.CharField(max_length=11, blank=True) # Use's videos native resolution if not specified/blank
+    recording_format = models.CharField(max_length=4, blank=True) # Same as above for mp4, DO NOT INCLUDE . IN DB!!
+    # Validator below to limit bitrate usage, god please have mercy on our DBs
+    recording_bitrate = models.PositiveSmallIntegerField(default=28, validators=[MaxValueValidator(32)])
+    
 
     class Meta:
         verbose_name = "FFmpeg Config"
@@ -31,7 +37,8 @@ class FFmpegConfig(models.Model):
     def save(self, *args, **kwargs):
         # We round to save only integers to db
         self.recording_duration = max(int(round(self.recording_duration)), 1)  # 1 is for minimum value
+        self.recording_bitrate = max(int(round(self.recording_bitrate)), 18) # Less than 18 would make each files be tooooo big (>w>)
         super().save(*args, **kwargs)
     
     def __str__(self):
-        return f"Recording Duration: {self.recording_duration} minutes / More future configs coming"
+        return f"Saved FFmpeg Recording Configuration - Duration: {self.recording_duration} minutes | Resolution: {self.recording_resolution} | Bitrate: {self.recording_bitrate} | Format: {self.recording_format}"
